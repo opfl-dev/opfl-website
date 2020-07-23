22f28d577276d8e1e3cbb1538479f5f0ad52443f
Andre Telfer <telfer006@gmail.com>
2020-07-23 13:13:27 -0400

    added local port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const process = require('process');
 
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 app.use(express.static('public'))
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
